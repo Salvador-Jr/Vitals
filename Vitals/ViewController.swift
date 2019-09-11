b6607a3b9733828df11a449856adb62cb6f65a44
Salvador Rodriguez <salvador.rodriguez01@sjsu.edu>
2019-09-10 21:08:37 -0700

    Initial Commit

---
+++ Vitals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vitals
+//
+//  Created by Salvador Rodriguez on 9/10/19.
+//  Copyright © 2019 009252542SalvadorRodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
